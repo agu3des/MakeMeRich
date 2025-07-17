b93b4fd32ece4d6101957a326404fdc23346d9d2
Ananda Guedes <anandaguedesdoo@gmail.com>
2025-07-17 16:26:45 -0300

    repository e service de categoria

---
+++ src/main/java/br/edu/ifpb/pweb2/makemerich/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.makemerich.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ifpb.pweb2.makemerich.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    List<Categoria> findByAtivaTrueOrderByNaturezaAscOrdemAsc();
+}
