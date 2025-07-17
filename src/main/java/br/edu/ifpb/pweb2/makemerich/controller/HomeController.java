09457128fa5cf1799058b1c3735378f97d9c044a
Ananda Guedes <anandaguedesdoo@gmail.com>
2025-07-17 16:46:55 -0300

    index, layouts e homeController

---
+++ src/main/java/br/edu/ifpb/pweb2/makemerich/controller/HomeController.java
@@ -0,0 +1,19 @@
+package br.edu.ifpb.pweb2.makemerich.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String showHomePage() {
+        return "index";
+    }
+
+    @ModelAttribute("menu")
+    public String selectMenu() {
+        return "home";
+    }
+}
