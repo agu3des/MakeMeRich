afdc1a85cee8b6e43cd4ace5296c4a23a647b523
l-e-t-i-c-i-a <proj3705@gmail.com>
2025-07-17 16:40:14 -0300

    Inicializador Correntista e PasswordUtil

---
+++ src/main/java/br/edu/ifpb/pweb2/makemerich/util/PasswordUtil.java
@@ -0,0 +1,23 @@
+package br.edu.ifpb.pweb2.makemerich.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public abstract class PasswordUtil {
+    public static String hashPassword(String plainTextPassword) {
+        return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
+    }
+
+    public static boolean checkPass(String plainPassword, String hashedPassword) {
+        if (BCrypt.checkpw(plainPassword, hashedPassword))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        String senha = "123";
+        System.out.println(PasswordUtil.hashPassword(senha));
+    }
+
+
+}
