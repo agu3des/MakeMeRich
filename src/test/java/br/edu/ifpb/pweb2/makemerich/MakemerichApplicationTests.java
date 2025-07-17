38e5b8e737ef097047f2986f05abeffd5bf25675
Ananda Guedes <anandaguedesdoo@gmail.com>
2025-07-17 15:43:43 -0300

    inicio do projeto

---
+++ src/test/java/br/edu/ifpb/pweb2/makemerich/MakemerichApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.makemerich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemerichApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
